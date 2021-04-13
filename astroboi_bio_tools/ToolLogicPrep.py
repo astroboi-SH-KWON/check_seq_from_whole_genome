bf0f2a1d5c6aba22ddd547a9e3ffa9ebb4c86afb
astroboi-lab <terry007x@korea.ac.kr>
2021-04-13 16:40:48 +0900

    sort integer

--- astroboi_bio_tools/ToolLogicPrep.py
+++ astroboi_bio_tools/ToolLogicPrep.py
@@ -6,6 +6,6 @@ class ToolLogicPreps:
 
     def sort_list_by_ele(self, data_list, ele_idx, up_down_flag=True):
         result_list = []
-        for tmp_arr in sorted(data_list, key=lambda tmp_arr: tmp_arr[ele_idx], reverse=up_down_flag):
+        for tmp_arr in sorted(data_list, key=lambda tmp_arr: int(tmp_arr[ele_idx]), reverse=up_down_flag):
             result_list.append(tmp_arr)
         return result_list
